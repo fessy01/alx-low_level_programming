95fe9c2b4aa9dc3cf654a04360052e2778fca9b0
FESSY <olaoluwaoni1@gmail.com>
2022-09-09 21:58:39 -0700

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,9 +1,8 @@
 
-
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 00 to 99.
+ * main - Prints numbers between 00 to 89.
  *
  * Return: Always 0 (Success)
  */
@@ -19,14 +18,17 @@ int main(void)
 		i = 48;
 		while (i < 58)
 		{
-			putchar(e);
-			putchar(i);
-			if (i == 57 && e == 57)
+			if (e != i && e < i)
 			{
-				break;
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
 			}
-			putchar(',');
-			putchar(' ');
 			i++;
 		}
 		e++;
