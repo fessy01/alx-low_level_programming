ab3fc4a611a879273479438b72eb5adac4e98732
FESSY <olaoluwaoni1@gmail.com>
2022-09-13 18:30:47 -0700

    fixed error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,8 +4,7 @@
  * print_alphabet - Prints the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
- */
-void print_alphabet(void)
+ */int main(void)
 {
 	int i;
 
@@ -14,4 +13,5 @@ void print_alphabet(void)
 		_putchar(i);
 	}
 	_putchar('\n');
+	return (0);
 }
