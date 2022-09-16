e67800b4f8c536b741fefe0fb8d160eed9214b8b
FESSY <olaoluwaoni1@gmail.com>
2022-09-15 18:50:05 -0700

    first commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function thta prints if integer is positive or nagative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_nagative(i);
+
+	return (0);
+}
