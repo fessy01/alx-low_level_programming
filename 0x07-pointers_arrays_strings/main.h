17aced579333ba600001560ea900e4352ec45b3d
FESSY <olaoluwaoni1@gmail.com>
2022-09-27 18:48:08 -0700

    Adding 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,6 +13,7 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int main(void);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 
 #endif /* MAIN_H */
 
